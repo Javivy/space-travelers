2e43c1cac118657aa0b4e5b04817c622e6544a67
joker69216 <hamaraad883@gmail.com>
2022-11-17 15:21:11 +0300

    fix eslinter errors

--- src/Components/Pages.test.js
+++ src/Components/Pages.test.js
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import store from '../Redux/configureStore';
+import store from '../Redux/configureStore.js';
 import App from '../App';
 
 describe('Test the Reservations', () => {
