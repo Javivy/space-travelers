971a3f2df00eb6abb4e734a1f393af023a12228d
joker69216 <hamaraad883@gmail.com>
2022-11-17 15:25:35 +0300

    fix eslinter errors

--- src/Components/Pages.test.js
+++ src/Components/Pages.test.js
@@ -1,50 +1,49 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { Provider } from "react-redux";
-import store from "../Redux/configureStore";
-import App from "../App";
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import store from '../Redux/configureStore';
+import App from '../App';
 
-describe("Test the Reservations", () => {
-  test("Test the App component", () => {
+describe('Test the Reservations', () => {
+  test('Test the App component', () => {
     render(
       <Provider store={store}>
         <App />
-      </Provider>
+      </Provider>,
     );
     const linkElement = screen.getByText(/Space Travelers/i);
     expect(linkElement).toBeInTheDocument();
   });
 
-  test("renders the reserved missions page", () => {
+  test('renders the reserved missions page', () => {
     render(
       <Provider store={store}>
         <App />
-      </Provider>
+      </Provider>,
     );
-    fireEvent.click(screen.getByText("My Profile"));
-    expect(screen.getByText("My Rockets")).toBeInTheDocument();
+    fireEvent.click(screen.getByText('My Profile'));
+    expect(screen.getByText('My Rockets')).toBeInTheDocument();
   });
 
-  test("renders the reserved missions page", () => {
+  test('renders the reserved missions page', () => {
     render(
       <Provider store={store}>
         <App />
-      </Provider>
+      </Provider>,
     );
-    fireEvent.click(screen.getByText("My Profile"));
-    expect(screen.getByText("My Missions")).toBeInTheDocument();
+    fireEvent.click(screen.getByText('My Profile'));
+    expect(screen.getByText('My Missions')).toBeInTheDocument();
   });
 });
 
-describe("Test the Reservations", () => {
-  test("Test the App component", () => {
+describe('Test the Reservations', () => {
+  test('Test the App component', () => {
     const rendered = render(
       <Provider store={store}>
         <App />
-      </Provider>
+      </Provider>,
     );
 
     expect(rendered).toMatchSnapshot();
   });
 });
-
